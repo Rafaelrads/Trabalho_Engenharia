3bbd915274094d36eb60ea118ebc25e800e6ddd5
Rafaelrads <rafaelaraujo.1998@gmail.com>
2015-09-10 10:19:49 -0300

    Hello usa greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Rafael Araújo (rafaelaraujo.1998@gmail.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
